c4d602666f954e102e84a80ef5408149d1c28603
Galactuuus <galactuuus@gmail.com>
2021-05-19 00:13:29 +0200

    Clean comments

--- app/Http/Controllers/BanController.php
+++ app/Http/Controllers/BanController.php
@@ -83,7 +83,7 @@ class BanController extends Controller
 
         $isBanned = Ban::where('party_id', $partyId)->where('user_id', $user)->first();
 
-        if(!$isBanned) return response()->json(['message' => 'No hay ningun baneo del usuario en dicha party'], 400);
+        if(!$isBanned) return response()->json(['message' => 'El usuario no se encuentra baneado de esa party'], 404);
 
         Ban::where('party_id', $partyId)->where('user_id', $user)->delete();
 
